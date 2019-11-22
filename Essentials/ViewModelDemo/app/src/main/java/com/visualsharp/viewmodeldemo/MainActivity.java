fedc97761a822a35afa08603d6d3c3b73eab453a
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2019-11-22 16:55:39 +0800

    Add fragment viewmodel

---
+++ Essentials/ViewModelDemo/app/src/main/java/com/visualsharp/viewmodeldemo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.visualsharp.viewmodeldemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.visualsharp.viewmodeldemo.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ Essentials/ViewModelDemo/app/src/main/java/com/visualsharp/viewmodeldemo/ui/main/MainViewModel.java
@@ -0,0 +1,18 @@
+package com.visualsharp.viewmodeldemo.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    private static final Float usd_to_eu_rate = 0.74F;
+    private String dollarText="";
+    private Float result = 0F;
+
+    public void setAmount(String value){
+        this.dollarText = value;
+        result = Float.valueOf(dollarText)*usd_to_eu_rate;
+    }
+
+    public Float getResult(){
+        return result;
+    }
+}
